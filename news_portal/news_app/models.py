946e3ff1c458bc048e19c9a628c45fd22967f041
111 <qwerty@example.com>
2026-05-05 10:53:19 +0500

    first_step

--- news_portal/news_app/models.py
+++ news_portal/news_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.CharField(max_length=300)
+    content = models.CharField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
